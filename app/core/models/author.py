5f6d3af443a6d9a354607f52918c956a8c72cfcf
lukavskym <demol.cv@gmail.com>
2024-12-11 00:10:18 +0300

    Изменить тип поля first_name на str, вместо int

--- app/core/models/author.py
+++ app/core/models/author.py
@@ -6,6 +6,6 @@ from sqlalchemy.orm import Mapped, mapped_column
 from .base import Base
 
 class Author(Base):
-    first_name: Mapped[int]
+    first_name: Mapped[str]
     last_name: Mapped[str]
     birthdate: Mapped[datetime]

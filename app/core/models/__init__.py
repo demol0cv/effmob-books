88eec4bc3bdca8a392182b56dba2c781e0d9248f
lukavskym <demol.cv@gmail.com>
2024-12-11 20:42:25 +0300

    Добавить импорты

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -1,7 +1,13 @@
 __all__ = [
     "db_helper",
     "Base",
+    "Book",
+    "Author",
+    "Borrow",
     ]
 
 from .db_helper import db_helper
 from .base import Base
+from .book import Book
+from .author import Author
+from .borrow import Borrow

40bb1c9cc7c7484d417f9385aece6e8cd07c3a5b
lukavskym <demol.cv@gmail.com>
2024-12-12 19:31:45 +0300

    Реализовать методы для взаимодействия с БД для books

--- app/api/booksrouter.py
+++ app/api/booksrouter.py
@@ -1,35 +1,85 @@
+from typing import Annotated
 
-
+import sqlalchemy.exc
 from fastapi import APIRouter
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from api.crud import BooksCrud
+from core.errors import APIException
+from core.models import db_helper, Book
+from core.schemas.book import BookRead, BookCreate, BookBase, BookUpdate
 from core.schemas.error import ErrorBase
+from fastapi import APIRouter, Depends, HTTPException
 
 router = APIRouter(tags=["Books"])
 router.responses = {
     404: {"model": ErrorBase, "description": "Not Found"}
 }
-authors = ["author1", "author2", "author3", "author4", "author5"]
 
-books = ["book1", "book2", "book3", "book4", "book5"]
 
-@router.get("")
-async def get_books_list():
+@router.get("", response_model=list[BookRead])
+async def get_books_list(
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+        offset: int = 0,
+        limit: int = 15,
+):
+    books = await BooksCrud.get_all_books(
+        session=session,
+        offset=offset,
+        limit=limit,
+    )
     return books
 
-@router.get("/{id}")
-async def get_book_info(id: int):
-    return books[id]
+@router.get("/{book_id}", response_model=BookRead)
+async def get_book_info(
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+        book_id: int,
+):
+    try:
+        book = await BooksCrud.get_one_book(
+            session=session,
+            book_id=book_id,
+        )
+        return book
+    except sqlalchemy.exc.NoResultFound:
+        raise APIException(
+            code=404,
+            error="Book not found",
+            details=f"Book with id={book_id} not found"
+        )
 
-@router.post("/{book_name}")
-async def add_book(book_name: str):
-    if book_name not in books:
-        books.append(book_name)
+@router.post("", response_model=BookCreate)
+async def add_book(
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+        book: BookCreate,
+) -> Book:
+    book = await BooksCrud.create_book(
+        session=session,
+        book_create=book,
+    )
+    return book
 
-@router.put("")
-async def update_book(id: int, new_name: str):
-    books[id] = new_name
 
-@router.delete("{id}")
-async def remove_book(id: int):
-    if id >=0 and id < len(books):
-        return books.pop(id)
+@router.put("/{book_id}", response_model=BookBase)
+async def update_book(
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+        book_id: int,
+        book_update: BookUpdate,
+):
+    book = await BooksCrud.update_book(
+        session=session,
+        book_id=book_id,
+        book_update=book_update,
+    )
+    return book
+
+@router.delete("{book_id}")
+async def remove_book(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    book_id: int,
+    ):
+    book = await BooksCrud.remove_book(
+        session=session,
+        book_id=book_id,
+    )
+    return book
--- app/api/crud/books.py
+++ app/api/crud/books.py
@@ -1,32 +1,90 @@
+import abc
 from typing import Sequence
-from sqlalchemy import select
+from sqlalchemy import select, update, delete
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.errors import APIException
 from core.models import Book
-from core.schemas.book import BookCreate
+from core.schemas.book import BookCreate, BookUpdate
 
+__all__ = [
+    "BooksCrud"
+]
+class BooksCrud(abc.ABC):
+    async def get_all_books(
+            session: AsyncSession,
+            offset: int = 0,
+            limit: int = 15
+    ) -> Sequence[Book]:
+        query = select(Book).order_by(Book.name).offset(offset).limit(limit)
+        result = await session.scalars(query)
+        return result.all()
 
-async def get_all_books(
-        session: AsyncSession,
-        offset: int = 0,
-        limit: int = 15
-) -> Sequence[Book]:
-    query = select(Book).order_by(Book.name).offset(offset).limit(limit)
-    result = await session.scalars(query)
-    return result.all()
+    async def get_one_book(
+            session: AsyncSession,
+            book_id: int,
+    ) -> Book:
+        query = select(Book).where(Book.id == book_id)
+        result = await session.scalars(query)
+        return result.one_or_none()
 
-async def get_one_book(
-        session: AsyncSession,
-        book_id: int,
-) -> Book:
-    query = select(Book).where(Book.id == book_id)
-    result = await session.scalars(query)
-    return result.one()
+    async def create_book(
+            session: AsyncSession,
+            book_create: BookCreate,
+    ) -> Book:
+        book = Book(**book_create.model_dump())
+        session.add(book)
+        await session.commit()
+        return book
 
-async def create_book(
-        session: AsyncSession,
-        book_create: BookCreate,
-) -> Book:
-    book = Book(**book_create.model_dump())
-    session.add(book)
-    await session.commit()
-    return book
+    async def update_book(
+            session: AsyncSession,
+            book_id: int,
+            book_update: BookUpdate,
+    ):
+
+        result = await session.execute(
+            select(Book).where(Book.id == book_id)
+        )
+        book_for_update = result.scalar_one_or_none()
+        if not book_for_update:
+            raise APIException(
+                code=404,
+                error="Book not found",
+                details=f"Книга с ID = {book_id} не найдена"
+            )
+        new_data = book_update.model_dump(exclude_unset=True)
+        if not new_data:
+            raise APIException(
+                code=400,
+                error="No data for update",
+                details=f"Нет данных для обновления"
+            )
+        for key, value in new_data.items():
+            setattr(book_for_update, key, value)
+        await session.commit()
+        result = await session.execute(
+            select(Book).where(Book.id == book_id)
+        )
+        result = result.scalar_one_or_none()
+        return result
+    
+    async def remove_book(
+            session: AsyncSession,
+            book_id: int
+            ) -> Book:
+        """
+        docstring
+        """
+        select_query = select(Book).where(Book.id == book_id)
+        result = await session.execute(select_query)
+        book = result.scalar_one_or_none()
+        if book is not None:
+            delete_query = delete(Book).where(Book.id == book_id)
+            await session.execute(delete_query)
+            await session.commit()
+        
+        return book
+        
+        #return result.scalar_one_or_none()
+

0475585aed04578196533e75195cc20258b2da3d
lukavskym <demol.cv@gmail.com>
2024-12-15 18:47:09 +0300

    Реализовать базовый класс CRUD
    Для реализации подхода DRY реализовать базовый класс CRUDBase для уменьшения повторяемости кода

---
+++ app/api/crud/basecrud.py
@@ -0,0 +1,62 @@
+from sqlalchemy import delete
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from typing import List, Optional, Sequence
+from sqlalchemy.orm import joinedload
+from core.models.base import Base
+from core.schemas.base import ItemBase
+
+class CRUDBase:
+    def __init__(self, model:Base):
+        self.model = model
+
+    async def get_one_item(
+            self,
+            session: AsyncSession,
+            id: int,
+    ) -> Base | None:
+        query = select(self.model).where(self.model.id == id)
+
+        result = await session.execute(query)
+
+        item = result.scalar_one_or_none()
+        return item
+    
+
+    async def get_all_items(
+            self,
+            session: AsyncSession,
+            offset: int = 0,
+            limit: int = 15,
+    ) -> Sequence[Base]:
+        query = select(self.model).order_by(self.model.id).offset(offset).limit(limit)
+        result = await session.scalars(query)
+        return result.all()
+    
+    async def create_item(
+            self,
+            session: AsyncSession,
+            item_create: ItemBase,
+    ) -> Base:
+        item = ItemBase(**item_create.model_dump())
+        session.add(item)
+        await session.commit()
+        return item
+    
+    async def remove_item(
+            self,
+            session: AsyncSession,
+            id: int,
+    ) -> Base:
+        select_query = select(self.model).where(self.model.id == id)
+        result = await session.execute(select_query)
+        item = result.scalar_one_or_none()
+        if item is not None:
+            delete_query = delete(self.model).where(self.model.id == item.id)
+            await session.execute(delete_query)
+            await session.commit()
+        
+        return item
+        
+    

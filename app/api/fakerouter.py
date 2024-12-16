cfb85aa0d4a51e50b7639d1f9fa86b2f22ad62f8
lukavskym <demol.cv@gmail.com>
2024-12-16 16:33:44 +0300

    Реализовать эндпоинт для заполнения БД фейковыми данными

--- app/api/fakerouter.py
+++ app/api/fakerouter.py
@@ -1,8 +1,18 @@
+from typing import Optional
 from fastapi import APIRouter
+from pydantic import Field
+
+from fill_db import DbUtils
 
 
 router = APIRouter(tags=["Fake"])
 
-@router.get("/fake")
-async def fill_fake_data():
-    pass
+@router.get("/fake",
+            summary="Заполнить БД фэйковыми данными",
+            description="Эндпоинт заполняет БД фэйковыми данным книг и авторов."
+            )
+async def fill_fake_data(
+    authors_count: Optional[int] = None,
+    books_count: Optional[int] = None,
+):
+    DbUtils().fill_fake_data(authors_count=authors_count, books_count=books_count)
